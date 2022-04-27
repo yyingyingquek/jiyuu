1dc8ae37597b9069f8b4bed317a97e4f14aadeda
yingyingquek <quekyingying26@gmail.com>
2022-04-27 15:26:51 +0800

    update for tailwind, router-6, axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="flex flex-col items-center mt-16">
+        <h1 className="text-4xl text-center">Trying tailwindcss!</h1>
+        <br />
+        <button class="bg-black hover:bg-gray-500 text-white font-bold py-2 px-2 rounded-full">
+          Test button
+        </button>
+      </div>
     </div>
   );
 }
