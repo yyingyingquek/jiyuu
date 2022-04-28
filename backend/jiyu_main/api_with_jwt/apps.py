8e01bb0efd0a822b91cb4888f80ad33e1687564c
yingyingquek <quekyingying26@gmail.com>
2022-04-28 15:39:52 +0800

    api_with_jwt

---
+++ backend/jiyu_main/api_with_jwt/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiWithJwtConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api_with_jwt'
--- backend/jiyu_main/jiyu_main/urls.py
+++ backend/jiyu_main/jiyu_main/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('main_app.urls')),
+    path('jwt_api/', include('api_with_jwt.urls'))
 ]
